20e296a8adfe49cdd0dc197f5d21262760b60bb9
Oleg Strizhechenko <oleg.strizhechenko@gmail.com>
2016-06-10 15:54:13 +0600

    Скрипт для поиска самых говнистых bash-скриптов в директории

---
+++ govno.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -eu
+
+script_list() {
+	grep -r '#!/bin/bash' "${1:-.}" 2>/dev/null | cut -d ':' -f1
+}
+
+script_check() {
+	echo -n "$1 "
+	shellcheck "$1" | wc -l | tr -d ' '
+}
+
+directory_lint() {
+	script_list "$@" | while read script; do
+		script_check "$script"
+	done | sort -nrk2
+}
+
+directory_lint "$@"
